bb768f54eeda8ed6dfb1e29b8c591af1be080c33
Ítalo Rodrigo <italo.computation@gmail.com>
2024-07-18 22:23:31 -0400

    Updates
    - main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from src.ExtractDataOmeka import ExtractDataOmeka
 from src.helper.Helper import Helper
-import json
-
 
 endpoint = 'http://repositorio.itegam.org.br/api'
 outputfile = 'output.csv'
@@ -14,7 +12,6 @@ if __name__ == '__main__':
     print(msg)
     ##
     response, content = extractomekaobj.request()   
-    print(json.dumps(str(content), indent=4)) 
     # print(Helper.showJSON(response))
-    # print(Helper.showJSON(content))
+    print(Helper.showJSON(content))
     
--- src/helper/Helper.py
+++ src/helper/Helper.py
@@ -1,6 +1,11 @@
 import json
 
+from pprint import pprint
+
 class Helper:
     
     def showJSON(data):
         return (json.dumps(data, indent=4))
+    
+    def showPrint(data):
+        return pprint(data, width=40)
